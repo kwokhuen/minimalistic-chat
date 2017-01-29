e071767affb58ef56ad9a9db768e51a1b28f7165
kwokhuen <benjaminkwokhuen@gmail.com>
2017-01-29 13:39:48 -0800

    format time and add animation

--- public/js/index.js
+++ public/js/index.js
@@ -9,24 +9,36 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('New Message', message);
-  var div = $('<div class="chat__each_message"></div>')
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var div1 = $('<div class="chat__each_message animated fadeInUp"></div>');
+  var div2 = $('<div class="chat__body_container"></div>');
+  var p1 = $('<p class="comment-user"></p>');
+  var p2 = $('<p class="comment-text"></p>');
+  var p3 = $('<p class="comment-timestamp"></p>');
   var li = $('<li></li>');
-  li.append(`<p class="comment-user">${message.from}</p>`);
-  li.append(`<p class="comment-text">${message.text}</p>`);
-  div.append(li);
-  $('#messages').append(div);
+  p1.text(message.from);
+  p2.text(message.text);
+  p3.text(formattedTime);
+  div2.append(p2).append(p3);
+  li.append(p1).append(div2);
+  div1.append(li);
+  $('#messages').append(div1);
 });
 
 socket.on('newLocationMessage', function(message) {
-  console.log('New Location Message', message);
-  var div = $('<div class="chat__each_message"></div>')
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var div = $('<div class="chat__each_message animated fadeInUp"></div>');
   var li = $('<li></li>');
+  var p1 = $('<p class="comment-user"></p>');
+  var p2 = $('<p class="comment-text"></p>');
+  var p3 = $('<p class="comment-timestamp"></p>');
   var a = $('<a target="_blank">My current location</a>');
   a.attr('href', message.url);
-  li.append(`<p class="comment-user">${message.from}</p>`);
-  $(`<p class="comment-user"></p>`).append(a).appendTo(li);
-  div.append(li)
+  p1.text(message.from);
+  p2.append(a);
+  p3.text(formattedTime);
+  li.append(p1).append(p2).append(p3);
+  div.append(li);
   $('#messages').append(div);
 });
 
@@ -49,16 +61,16 @@ locationButton.on('click', function() {
     return alert('Geolocation not supported by your browser.');
   }
 
-  locationButton.attr('disabled', 'disabled').text('Sending Location...');
+  locationButton.addClass('animated infinite pulse').attr('disabled', 'disabled').text('Sending Location...');
 
   navigator.geolocation.getCurrentPosition(function(position) {
-    locationButton.removeAttr('disabled').text('Send Location');
+    locationButton.removeAttr('disabled').removeClass('animated infinite pulse').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
-    locationButton.removeAttr('disabled').text('Send Location');
+    locationButton.removeAttr('disabled').removeClass('animated infinite pulse').text('Send Location');
     alert('Unable to fetch location');
   });
 });
