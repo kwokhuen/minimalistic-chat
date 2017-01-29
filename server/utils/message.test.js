a807c7ecc97fa3cfbf2b4c0ac93cf2a98bff8aff
kwokhuen <benjaminkwokhuen@gmail.com>
2017-01-28 17:09:34 -0800

    create genrate message utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,13 @@
+var expect = require('expect');
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate a correct message object', () => {
+    var from = 'benjamin';
+    var text = 'hello';
+    var message = generateMessage(from, text);
+    expect(message.from).toBe(from);
+    expect(message.text).toBe(text);
+    expect(message.createdAt).toBeA('number');
+  })
+});
