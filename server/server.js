691719523047c7612740256c6cc238a2a3699351
kwokhuen <benjaminkwokhuen@gmail.com>
2017-01-30 19:44:42 -0800

    send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ io.on('connection', (socket) => {
     }
 
     socket.join(params.room);
-    users.removeUser(socket.id);
+    // users.removeUser(socket.id);
     users.addUser(socket.id, params.name, params.room);
 
     io.to(params.room).emit('updateUserList', users.getUserList(params.room));
@@ -39,12 +39,18 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message, callback) => {
     console.log('Create Message', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
     callback(); // This callback function is called acknowlendgement.
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
