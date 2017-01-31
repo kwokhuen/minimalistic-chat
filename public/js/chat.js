74dfedb7e15bbc48bbf8485c01b4e5b3baf99fd4
kwokhuen <benjaminkwokhuen@gmail.com>
2017-01-31 00:51:50 -0800

    add screenshot

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,7 +18,6 @@ function scrollToBottom() {
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
   var params = $.deparam(window.location.search);
   socket.emit('join', params, function(err) {
     if (err) {
